2ad3d19392b0a156e3b9c261a4d77bdc21e36801
Dmytro <142408709+rustic-coder@users.noreply.github.com>
2023-10-01 15:27:42 +0200

    Add necessary files.

---
+++ main.py
@@ -0,0 +1,138 @@
+# Importing necessary libraries & modules
+import asyncio
+import json
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message, CallbackQuery, ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.markdown import hbold
+from weather_report import WeatherReport
+
+# Set logging level
+logging.basicConfig(level=logging.INFO)
+
+
+BOT_TOKEN = ""
+
+# Parsing bot token
+with open("config.json") as json_file:
+    BOT_TOKEN = json.load(json_file)["telegram_token"]
+
+# Unit system variable is responsible for the unit system to be used for weather information
+unit_system = ""
+
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
+
+# Create main bot control menu
+featuredCityListButton = KeyboardButton(text="🏅 Featured places (Available soon...)")
+changeUnitTypeButton = KeyboardButton(text="🌡 Units")
+setOrRemoveNotificationsButton = KeyboardButton(text="🔔 Notifications (Available soon...)")
+botControlMenuMarkup = ReplyKeyboardMarkup(keyboard=[[changeUnitTypeButton],
+                                                     [featuredCityListButton, setOrRemoveNotificationsButton]],
+                                           is_persistent=True, resize_keyboard=True)
+
+
+# Create inline unit system selection menu
+metricUnitButton = InlineKeyboardButton(text="🌡️ Imperial (°F, mi/h)", callback_data="imperial")
+imperialUnitButton = InlineKeyboardButton(text="🌡️ Metric (°C, m/s)", callback_data="metric")
+selectUnitTypeMarkup = InlineKeyboardMarkup(inline_keyboard=[[metricUnitButton, imperialUnitButton]])
+
+
+@dp.callback_query(lambda call: call.data in ["imperial", "metric"])
+async def proceedSelectedUnitType(call: CallbackQuery):
+    global unit_system
+
+    # Check which option has been chosen by user
+    if call.data == "imperial":
+
+        # If user has chosen imperial unit system then set
+        # imperial system as default and show appropriate message
+        unit_system = "imperial"
+        await call.message.answer(f"You've chosen {hbold('Imperial')} as primary unit!"
+                                  f"\nYou can change it later using {hbold('🌡 Units')}.")
+    elif call.data == "metric":
+
+        # If user has chosen metric unit system then set metric
+        # system as default and show appropriate message
+        unit_system = "metric"
+        await call.message.answer(f"You've chosen {hbold('Metric')} as primary unit system!"
+                                  f"\nYou can change it later using {hbold('🌡 Units')}.")
+
+    await call.message.answer("✅ Now you can send me any city, region or village to get latest weather info!",
+                              reply_markup=botControlMenuMarkup)
+
+
+@dp.message(CommandStart())
+async def greets(message: Message):
+
+    # Send greeting message when user sends /start to bot
+    await message.answer(f"Hello, {message.from_user.full_name}! Welcome to {hbold('GetWeather')} Bot!👋\n"
+                         f"To start, select unit system:", reply_markup=selectUnitTypeMarkup)
+
+
+@dp.message(lambda message: message.text == "🌡 Units")
+async def sendEditUnitSystemRequest(message: Message):
+
+    # Asking to select unit system in inline menu when user want to change it
+    await message.answer("Select unit system you prefer:", reply_markup=selectUnitTypeMarkup)
+
+
+@dp.message(lambda message: message.text.startswith("🔔 Notifications"))
+async def setNotificationsRequest(message: Message):
+
+    # Feature under development
+    await message.answer(
+        "We apologize for the inconvenience, but at the moment, this feature is in the development stage.☹.")
+
+
+@dp.message(lambda message: message.text.startswith("🏅 Featured places"))
+async def sendEditUnitSystemRequest(message: Message):
+
+    # Feature under development
+    await message.answer(
+        "We apologize for the inconvenience, but at the moment, this feature is in the development stage  ☹.")
+
+
+@dp.message()
+async def sendGetWeatherRequest(message: Message):
+
+    # Parse city/region name
+    city_name = message.text
+
+    if not unit_system:
+
+        # If user hasn't selected any unit system then asking him to do this
+        await message.answer("⚠ To start using bot, select unit system:", reply_markup=selectUnitTypeMarkup)
+    else:
+
+        # Create WeatherReport instance to receive and process weather info
+        weather = WeatherReport(city=city_name, unit_system=unit_system)
+
+        # Get weather info and catching errors to res variable
+        res = weather.getWeatherData(weather.URL())
+
+        if res:
+
+            # Send appropriate error message to user if there're any errors
+            await message.answer(res, reply_markup=botControlMenuMarkup)
+        else:
+
+            # Get formatted weather info to show it to user
+            weather = weather.beautify()
+
+            # Send desired city/region weather to user
+            await message.answer(weather, reply_markup=botControlMenuMarkup)
+
+
+async def main() -> None:
+
+    # Start bot
+    bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+    await dp.start_polling(bot)
+
+
+# Run main bot function
+asyncio.run(main())
---
+++ weather_report.py
@@ -0,0 +1,132 @@
+# Import necessary libraries and modules
+from urllib import request, parse, error
+from aiogram.utils.markdown import hbold
+import json
+import flag
+
+
+class WeatherReport:
+    def __init__(self, city, unit_system):
+
+        # Initializing required variables
+        self.__weather = {}
+        city = parse.quote_plus(city)
+        self.city = city
+        self.unit_system = unit_system
+        self.__WEATHER_TOKEN = ""
+
+        with open("config.json") as json_file:
+            self.__WEATHER_TOKEN = json.load(json_file)["weather_token"]
+        self.__WEATHER_URL = f"https://api.openweathermap.org/data/2.5/weather"
+
+
+    def URL(self) -> str:
+
+        # Format API url for specific unit system & city|region
+        result_url = self.__WEATHER_URL + f"?q={self.city}"
+        result_url = result_url + f"&units={self.unit_system}"
+        result_url = result_url + f"&appid={self.__WEATHER_TOKEN}"
+
+        # Return final url
+        return result_url
+
+    def getWeatherData(self, query_url) -> None | str:
+        try:
+
+            # Try to obtain json info
+            response = request.urlopen(query_url)
+        except error.HTTPError as http_error:
+
+            # If any error occurred then return error message
+            match http_error.code:
+                case 401:
+                    return "❌ API Key Error!"
+                case 404:
+                    return "❌ Can't find weather data for this city."
+                case _:
+                    return "❌ Unknown search error!"
+
+        # Reading json data
+        data = response.read()
+
+        try:
+
+            # Try to JSON data to dictionary
+            self.__weather = json.loads(data)
+        except json.JSONDecodeError:
+
+            # If error occured then return error message
+            return "❌ Couldn't read the server response!"
+
+    def getWindDirection(self) -> str:
+
+        # Obtain wind degree
+        wind_deg = self.__weather["wind"]["deg"]
+
+        # Simple if-elif-elif-else block to get wind direction
+        if 315 < wind_deg or wind_deg <= 45:
+            return "🧭 North"
+        elif 45 < wind_deg <= 135:
+            return "🧭 East"
+        elif 135 < wind_deg <= 225:
+            return "🧭 South"
+        else:
+            return "🧭 West"
+
+    def getBasicForecast(self) -> str:
+
+        # Obtain brief weather description
+        weather_main = self.__weather["weather"][0]["main"]
+
+        weather_main = weather_main.lower()
+
+        # Generate beautified output
+        match weather_main:
+            case "thunderstorm":
+                weather_main = f"it's a {weather_main} outside ⛈️"
+            case "drizzle":
+                weather_main = f"just a light {weather_main} 🌧️"
+            case "rain":
+                weather_main = f"it's {weather_main}ing 🌧️"
+            case "snow":
+                weather_main = f"let it {weather_main} ❄️"
+            case "mist" | "smoke" | "haze" | "dust" | "sand" | "ash":
+                weather_main = f"it's too {weather_main}y 🌫️"
+            case "fog":
+                weather_main = f"it's too {weather_main}gy 🌫️"
+            case "squall":
+                weather_main = f"it's a {weather_main}! 🌪"
+            case "tornado":
+                weather_main = f"woah, a {weather_main}! 🌪"
+            case "clouds":
+                weather_main = f"such a cloudy weather ☁️"
+            case "clear":
+                weather_main = f"it's {weather_main} outside! Time to go for a walk! ☀️"
+            case _:
+                weather_main = f"hmm.. Sorry but I don't know what's going on outside ☹."
+
+        # Return beautified output
+        return weather_main
+
+    def beautify(self) -> str:
+
+        # Obtain wind direction
+        wind_direction = self.getWindDirection()
+
+        # Obtain country flag emoji
+        country_code = flag.flag(self.__weather["sys"]["country"])
+
+        # Obtain units to use in output
+        wind_speed_unit = "m/s" if self.unit_system == "metric" else "mi/h"
+        temperature_unit = "°C" if self.unit_system == "metric" else "°F"
+
+        # Beautify resulting weather info
+        result = f"""
+{hbold('🏙️ City:')} {self.__weather["name"]}, {self.__weather["sys"]["country"]} {country_code}
+{hbold('🌡 Current:')} {self.__weather["main"]["temp"]} {temperature_unit}
+{hbold('🌡️ Feels like:')} {self.__weather["main"]["feels_like"]} {temperature_unit}
+{hbold('💨 Wind:')} {wind_direction}, {self.__weather["wind"]["speed"]} {wind_speed_unit}
+In brief, {self.getBasicForecast()}"""
+
+        # Return beautified output
+        return result
